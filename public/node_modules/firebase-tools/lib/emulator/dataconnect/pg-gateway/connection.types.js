e69da196e9a4ff8579d8e9cac27d73413bb29065
Nicolasguerra9 <guerraniko92@gmail.com>
2024-10-23 17:14:17 +0200

    retocado

--- public/node_modules/firebase-tools/lib/emulator/dataconnect/pg-gateway/connection.types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ServerStep = void 0;
-exports.ServerStep = {
-    AwaitingInitialMessage: 'AwaitingInitialMessage',
-    PerformingAuthentication: 'PerformingAuthentication',
-    ReadyForQuery: 'ReadyForQuery',
-};
